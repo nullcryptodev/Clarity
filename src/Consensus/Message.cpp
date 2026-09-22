d6e1b4a0ba20cfa05e3a6eb507d9df2738165cd6
nullcrypto (Jay) <nullcryptodev@proton.me>
2026-09-22 09:03:48 +0100

    Fix includes

--- src/Consensus/Message.cpp
+++ src/Consensus/Message.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "ConsensusMessage.h"
+#include "Message.h"
 
 #include "Core/Block.h"
 #include "Crypto/Blake2b.h"
